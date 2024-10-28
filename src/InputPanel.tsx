72b4b167d92e50ce1234bb137fd599db1f3645f0
Samu902 <samuelemarrani@gmail.com>
2024-10-28 23:17:15 +0100

    bozza file input

--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -7,16 +7,18 @@ import Button from 'react-bootstrap/Button';
 
 export default function InputPanel() {
     
+    const [filename, setFilename] = useState(null);
+
     return (
         <Container className='text-center px-5 py-4 border border-3 rounded-2'>
             <h4 className='mb-4'>Carica l'immagine che vuoi processare.</h4>
             <img src='' width={512} height={256} />
             <Row className='mt-4'>
                 <Col xs={9}>
-                    <Form.Control type='file' />
+                    <Form.Control type='file' value={filename} onChange={e => setFilename(e.currentTarget.value)}/>
                 </Col>
                 <Col xs={3}>
-                    <Button type='button' className='btn btn-primary'>Processa!</Button>
+                    <Button type='button' className='btn btn-primary' onClick={() => alert(filename)}>Processa!</Button>
                 </Col>
             </Row>
         </Container>
