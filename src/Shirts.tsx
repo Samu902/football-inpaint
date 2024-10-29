ddec828b62ae43232465adc593c188f93f5e54ca
Samu902 <samuelemarrani@gmail.com>
2024-10-29 12:45:19 +0100

    aggiunta squadra fiorentina

--- src/Shirts.tsx
+++ src/Shirts.tsx
@@ -1,5 +1,5 @@
 import Team1 from '/img/shirts/juve.jpg';
-import Team2 from '/img/shirts/juve.jpg';
+import Team2 from '/img/shirts/fiorentina.png';
 import Team3 from '/img/shirts/juve.jpg';
 import Team4 from '/img/shirts/juve.jpg';
 import Team5 from '/img/shirts/juve.jpg';
@@ -7,7 +7,7 @@ import Team6 from '/img/shirts/juve.jpg';
 
 export const shirts = [
     { img: Team1, team: "Juventus" },
-    { img: Team2, team: "Juventus" },
+    { img: Team2, team: "Fiorentina" },
     { img: Team3, team: "Juventus" },
     { img: Team4, team: "Juventus" },
     { img: Team5, team: "Juventus" },
