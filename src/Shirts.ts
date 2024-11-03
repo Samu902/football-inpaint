fab4c0fe1419d4bc488b65ecf93bed5e7553e905
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-03 12:27:33 +0100

    shirts

--- src/Shirts.ts
+++ src/Shirts.ts
@@ -1,5 +1,5 @@
 import Team1 from '/img/shirts/juventus.jpg';
-import Team2 from '/img/shirts/fiorentina.png';
+import Team2 from '/img/shirts/fiorentina.jpg';
 import Team3 from '/img/shirts/inter.jpg';
 import Team4 from '/img/shirts/milan.jpg';
 import Team5 from '/img/shirts/napoli.jpg';
