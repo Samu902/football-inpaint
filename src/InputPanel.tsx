6b7683171a626bf11e984a0444f6afb86318b194
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-03 12:30:25 +0100

    logica react input image

--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import PlaceholderImage from '/img/placeholder.jpg'
 
 export default function InputPanel() {
-    
-    const [filename, setFilename] = useState(null);
+
+    const [file, setFile] = useState<File>();
+    const [image, setImage] = useState(null);
+
+    function onFileChange(e: ChangeEvent<HTMLInputElement>) {
+        if (!e.target.files)
+            return;
+
+        setFile(e.target.files[0])
+        if (e.target.files[0]) {
+            const reader = new FileReader();
+            reader.onloadend = () => setImage(reader.result);
+            reader.readAsDataURL(e.target.files[0]);
+        }
+        else {
+            setImage(null);
+        }
+    }
 
     return (
         <Container className='text-center px-5 py-4 border border-3 rounded-2'>
             <h4 className='mb-4'>Carica l'immagine che vuoi processare.</h4>
-            <img src='' width={512} height={256} />
+            <img src={image || PlaceholderImage} width={512} height={256} className='border border-3 rounded-3' />
             <Row className='mt-4'>
                 <Col xs={9}>
-                    <Form.Control type='file' value={filename} onChange={e => setFilename(e.currentTarget.value)}/>
+                    <Form.Control type='file' onChange={onFileChange} />
                 </Col>
                 <Col xs={3}>
-                    <Button type='button' className='btn btn-primary' onClick={() => alert(filename)}>Processa!</Button>
+                    <Button type='button' className='btn btn-primary' onClick={() => alert(file.name)}>Processa!</Button>
                 </Col>
             </Row>
         </Container>
