04a62f9b541501df4bf1d79a3f03447c7b0bda56
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-03 12:45:57 +0100

    pulizia app.tsx

--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import PlaceholderImage from '/img/placeholder.jpg'
+import PlaceholderImage from '/img/placeholder.jpg';
 
 export default function InputPanel() {
 
