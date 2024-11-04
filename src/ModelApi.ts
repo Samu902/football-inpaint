a83abc02a8a6a6e1e4a955bcb7c616e8a632955f
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-04 18:55:00 +0100

    chiamata api funzionante

--- src/ModelApi.ts
+++ src/ModelApi.ts
@@ -1,23 +1,37 @@
-class ModelApi {
+export default class ModelApi {
+    
+    team1: string = 'Juventus';
+    team2: string = 'Juventus';
+    inputImage: File = null;
 
-    async processImage(file: File, team1: string, team2: string): Promise<string> {
+    onProcessStart = () => { return };
+
+    onProcessEnd = (outputImage: string | ArrayBuffer) => { return };
+
+    async processImage(): Promise<void> {
         const formData = new FormData();
-        formData.append('file', file);
-        formData.append('team1', team1);
-        formData.append('team2', team2);
+        formData.append('file', this.inputImage);
+        formData.append('team1', this.team1);
+        formData.append('team2', this.team2);
 
         try {
+            this.onProcessStart();
+
             const response = await fetch('http://localhost:5000/process-image', {
                 method: 'POST',
                 body: formData,
             });
 
-            if (!response.ok)
-                throw new Error('Errore durante l\'upload dell\'immagine');
+            if (!response.ok) {
+                const json = await response.json();
+                throw new Error('API Flask error: ' + json.error);
+
+            }
 
             const blob = await response.blob();
-            const outputFileUrl = window.URL.createObjectURL(blob);
-            return outputFileUrl;
+            const reader = new FileReader();
+            reader.onloadend = () => this.onProcessEnd(reader.result);
+            reader.readAsDataURL(blob);
         } catch (error) {
             console.error('Error:', error);
             throw error;
