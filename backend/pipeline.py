149d7dcf19a5756f77db75991ab1d1f85b07c779
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-08 14:02:22 +0100

    implementato team classifier

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -1,12 +1,13 @@
 # stage 0
 import os
+import shutil
 import gdown
 
 # stage 1
 from ultralytics import YOLO
 from PIL import Image
 
-# stage 2
+# stage 2a
 from ultralytics import SAM
 import numpy as np
 import copy
@@ -24,26 +25,36 @@ import cv2 as cv
 def init(): # andrà prob spostato in dockerfile
     #global ready
 
-    os.makedirs('./roboflow-model', exist_ok=True)
-    os.makedirs('./sdxl_lora_weights', exist_ok=True)
-
-    os.makedirs('./data/stage_0', exist_ok=True)
-    os.makedirs('./data/stage_1', exist_ok=True)
-    os.makedirs('./data/stage_1/players', exist_ok=True)
-    os.makedirs('./data/stage_2', exist_ok=True)
-    os.makedirs('./data/stage_2/masks', exist_ok=True)
-    os.makedirs('./data/stage_2/cropped_masks', exist_ok=True)
-    os.makedirs('./data/stage_3', exist_ok=True)
-    os.makedirs('./data/stage_4', exist_ok=True)
+    clean_data_dir()
 
     gdown.download(id='103DgLujAKKLlfETz-rgDO0-ibvQh7evQ', output='./roboflow-model/best.pt')
     gdown.download_folder(id='1VvzOiPwhkv7fuK7P3IktuEzuXdnKg3Le', output='./sdxl_lora_weights')
 
     #ready = True
 
+def clean_data_dir():
+    shutil.rmtree('./data', ignore_errors=True)
+
+    os.makedirs('./data/stage_0', exist_ok=True)
+    os.makedirs('./data/stage_1', exist_ok=True)
+    os.makedirs('./data/stage_1/players', exist_ok=True)
+    os.makedirs('./data/stage_1/players_no_pad', exist_ok=True)
+    os.makedirs('./data/stage_2a', exist_ok=True)
+    os.makedirs('./data/stage_2a/masks', exist_ok=True)
+    os.makedirs('./data/stage_2a/cropped_masks', exist_ok=True)
+    os.makedirs('./data/stage_2b', exist_ok=True)
+    os.makedirs('./data/stage_2b/team_0', exist_ok=True)
+    os.makedirs('./data/stage_2b/team_1', exist_ok=True)
+    os.makedirs('./data/stage_3', exist_ok=True)
+    os.makedirs('./data/stage_4', exist_ok=True)
+
 def start(input_image: Image.Image, team1: str, team2: str):
     
     ## STAGE 0
+    # preparazione dell'ambiente prima dell'esecuzione dei modelli della pipeline
+
+    # clean data directory
+    clean_data_dir()
 
     # translate team codes
     team_dict = { 'Juventus': 'sqjvnts', 'Fiorentina': 'sqfrntn', 'Inter': 'sqntrxx', 'Milan': 'sqmlnxx', 'Napoli': 'sqnplxx', 'Roma': 'sqrmxxx' }
@@ -51,8 +62,7 @@ def start(input_image: Image.Image, team1: str, team2: str):
     team2_code = team_dict[team2]
 
     # setup env var
-    HOME = os.getcwd()
-    print(HOME)
+    print('working directory: ' + os.getcwd())
     my_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # save input image
@@ -75,7 +85,7 @@ def start(input_image: Image.Image, team1: str, team2: str):
     classes = result_stage_1.boxes.cls.tolist()         # class of each bounding box
     result_stage_1.save(filename=f"data/stage_1/result.jpg")
 
-    # Crop players from images (filter out referees, goalkeepers and balls)
+    # Crop players from images (filter out referees, goalkeepers and balls) with and without padding
     pad = 50
     i = 0
     for b in boxes:
@@ -83,11 +93,13 @@ def start(input_image: Image.Image, team1: str, team2: str):
             b = b.tolist()
             im = input_image.crop((b[0] - pad, b[1] - pad, b[2] + pad, b[3] + pad))
             im.save(f"data/stage_1/players/player_{i}.jpg")
+            im_no_pad = input_image.crop(b)
+            im_no_pad.save(f"data/stage_1/players_no_pad/player_{i}.jpg")
         i += 1
 
     # ----------------------------
 
-    ## STAGE 2
+    ## STAGE 2a
     # questo modello, guidato dalle bounding box fornite dal modello precedente, crea le maschere dei giocatori in due versioni: immagine integrale e ritagliata a bounding box
 
     # Load SAM2 tiny model (good quality and speed)
@@ -95,28 +107,63 @@ def start(input_image: Image.Image, team1: str, team2: str):
 
     # Run inference on input_image: segment inside bounding boxes
     i = 0
-    result_stage_2 = model(input_image, bboxes=copy.deepcopy(boxes))[0]     # deep copy to avoid array "contamination"
+    result_stage_2a = model(input_image, bboxes=copy.deepcopy(boxes))[0]     # deep copy to avoid array "contamination"
 
     # Process result
     i = 0
-    for m in result_stage_2.masks:
+    for m in result_stage_2a.masks:
         if classes[i] == 2:       # only players
             mfile = Image.fromarray((m.data.cpu().numpy().squeeze() * 255).astype(np.uint8)).convert('L')   # create mask image
-            mfile.save(fp=f"data/stage_2/masks/mask_{i}.jpg")                                               # save to disk
+            mfile.save(fp=f"data/stage_2a/masks/mask_{i}.jpg")                                               # save to disk
             b = boxes[i].tolist()
             cmfile = mfile.crop((b[0] - pad, b[1] - pad, b[2] + pad, b[3] + pad))                           # create cropped mask image                                                                          # display to screen
-            cmfile.save(fp=f"data/stage_2/cropped_masks/cropped_mask_{i}.jpg")                              # save to disk
+            cmfile.save(fp=f"data/stage_2a/cropped_masks/cropped_mask_{i}.jpg")                              # save to disk
         i += 1
 
     # ----------------------------
 
     ## STAGE 2b
-    # questo modello divide i giocatori in due squadre
+    # questo modello prima impara autonomamente le differenze tra le due squadre e poi divide i giocatori nei due gruppi
 
-    crops = []
-    # i crop glieli do io - oppure lo allena sul momento con i calciatori della foto in input?
+    # load player crops
+    i = 0
+    players_crops = []
+    for b in boxes:
+        if not os.path.isfile(f"data/stage_1/players_no_pad/player_{i}.jpg"):      # only players (other classes' images haven't been processed, so skip them)
+            i += 1
+            continue
+        im = Image.open(f"data/stage_1/players_no_pad/player_{i}.jpg")
+        players_crops.append(np.asarray(im))
+        i += 1
+
+    # fit TeamClassifier
     team_classifier = TeamClassifier(device=my_device)
-    team_classifier.fit(crops)
+    team_classifier.fit(players_crops)
+
+    # predict teams
+    team_ids = list(team_classifier.predict(players_crops))
+    print('team_ids: ' + str(team_ids))
+
+    # fill list with holes (goalkeepers, referees, ball) to pass correct indices to next steps
+    i = 0
+    for b in boxes:
+        if not os.path.isfile(f"data/stage_1/players_no_pad/player_{i}.jpg"):
+            team_ids.insert(i, None)
+        i += 1
+    print('team_ids (filled): \n' + '\n'.join([f'subject {i}\'s team: {t}' for (i, t) in enumerate(team_ids)]))
+
+    # save player crops to corresponding team folder to help debugging
+    i = 0
+    for b in boxes:
+        if not os.path.isfile(f"data/stage_1/players_no_pad/player_{i}.jpg"):      # only players (other classes' images haven't been processed, so skip them)
+            i += 1
+            continue
+        im = Image.open(f"data/stage_1/players_no_pad/player_{i}.jpg")
+        im.save(fp=f"data/stage_2b/team_{team_ids[i]}/player_{i}.jpg")
+        i += 1
+
+    # DEBUG
+    return input_image
 
     # ----------------------------
 
@@ -173,7 +220,7 @@ def start(input_image: Image.Image, team1: str, team2: str):
             continue
         init_image = Image.open(f"data/stage_1/players/player_{i}.jpg").convert('RGB').resize((1024, 1024))     # load and resize player image
         #init_image = Image.new("RGB", (1024, 1024), (15, 191, 88))
-        mask_image = dilated_mask(Image.open(f"data/stage_2/cropped_masks/cropped_mask_{i}.jpg").convert('L').resize(output_inpaint_size), 6) # load, dilate and resize mask
+        mask_image = dilated_mask(Image.open(f"data/stage_2a/cropped_masks/cropped_mask_{i}.jpg").convert('L').resize(output_inpaint_size), 6) # load, dilate and resize mask
         #mask_image = Image.new("L", (1024, 1024), 255)
         pipe.set_adapters("sq1")
         image = pipe(
@@ -207,7 +254,7 @@ def start(input_image: Image.Image, team1: str, team2: str):
         player_box = list(map(lambda b: round(b), player_box.tolist()))     # round box coordinates and convert to list
         # Apri l'immagine da incollare e la maschera per ritagliarla
         player_image = Image.open(f"data/stage_3/result_{i}.jpg").resize((player_box[2] - player_box[0] + pad * 2, player_box[3] - player_box[1] + pad * 2))
-        mask_image = dilated_mask(Image.open(f"data/stage_2/cropped_masks/cropped_mask_{i}.jpg").resize((player_box[2] - player_box[0] + pad * 2, player_box[3] - player_box[1] + pad * 2)), 2).convert('L')
+        mask_image = dilated_mask(Image.open(f"data/stage_2a/cropped_masks/cropped_mask_{i}.jpg").resize((player_box[2] - player_box[0] + pad * 2, player_box[3] - player_box[1] + pad * 2)), 2).convert('L')
         # Incolla l'immagine
         general_image.paste(player_image, (player_box[0] - pad, player_box[1] - pad), mask_image)
         i += 1
