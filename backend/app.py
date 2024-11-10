087d2822562fd0c68557788942b779ddfa5a957a
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-10 18:54:08 +0100

    endpoint di aggiornamento e finalizzazione

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,10 @@
 from flask import Flask, request, jsonify, send_file
-from celery import Celery
 from PIL import Image
 import traceback
-from pipeline import Pipeline
+import pipeline
+from my_util import PIL_to_base64
 
 app = Flask(__name__)
-celery = Celery(
-    'app',
-    broker='redis://127.0.0.1:6379/0'
-)
-
-#celery -A app.celery worker --loglevel=INFO
 
 cors_headers = { 
     'Access-Control-Allow-Origin': '*',
@@ -32,11 +26,11 @@ def home():
     except Exception as e:
         return jsonify({'error': str(e)}), 500, cors_headers
 
-@app.route('/process-image', methods=['POST'])
-def process_image():
+@app.route('/process-image/start', methods=['POST'])
+def process_image_start():
 
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 500, cors_headers
+    if 'input_image' not in request.files:
+        return jsonify({'error': 'No input image'}), 500, cors_headers
     
     if not request.form.get('team1'):
         return jsonify({'error': 'No team 1'}), 500, cors_headers
@@ -44,30 +38,54 @@ def process_image():
     if not request.form.get('team2'):
         return jsonify({'error': 'No team 2'}), 500, cors_headers
     
-    file = request.files['file']
+    input_image = request.files['input_image']
     team1 = request.form.get('team1')
     team2 = request.form.get('team2')
     
-    if file.filename == '':
+    if input_image.filename == '':
         return jsonify({'error': 'No selected file'}), 500, cors_headers
 
-    # Elaborazione dell'immagine
     try:
-        image = Image.open(file.stream)
-        processed_image = pipe.start(image, team1, team2)
-        processed_image.save('./last_processed.png', 'PNG')
+        image_base64 = PIL_to_base64(Image.open(input_image.stream))
+        task = pipeline.start_new_task.delay(image_base64, team1, team2)
     except Exception as e:
         with open('app.log', 'w+') as f:
             traceback.print_exc(file=f)
         return jsonify({'error': str(e)}), 500, cors_headers
-    
-    return send_file(
-        './last_processed.png',
-        mimetype='image/png',
-        as_attachment=True,
-        download_name='processed_image.png'
-    ), 200, cors_headers
+
+    return jsonify({'info': 'Your image processing request was enqueued successfully', 'task_id': task.id}), 202, cors_headers
+
+@app.route('/process-image/update/<task_id>', methods=['GET'])
+def process_image_update(task_id):
+    task = pipeline.celery.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        return jsonify({'status': 'Task is pending...'}), 202, cors_headers
+    elif task.state == 'PROGRESS':
+        return jsonify({'status': 'Task is in progress...'}), 202, cors_headers
+    elif task.state == 'SUCCESS':
+        return jsonify({'status': 'Task finished successfully, please call /finalize to get your result.'}), 200, cors_headers
+    elif task.state == 'FAILURE':
+        return jsonify({'status': 'Task failed for the following reason:\n' + str(task.traceback)}), 500, cors_headers
+    else:
+        return jsonify({'status': 'Unknown state...'}), 202, cors_headers
+
+@app.route('/process-image/finalize/<task_id>', methods=['GET'])
+def process_image_finalize(task_id):
+    try:
+        processed_image = pipeline.celery.AsyncResult(task_id).info
+        processed_image.save(fp='./last_processed.png')
+        return send_file(
+            './last_processed.png',
+            mimetype='image/png',
+            as_attachment=True,
+            download_name='processed_image.png'
+        ), 200, cors_headers
+    except Exception as e:
+        with open('app.log', 'w+') as f:
+            traceback.print_exc(file=f)
+        return jsonify({'error': str(e)}), 500, cors_headers
 
 if __name__ == '__main__':
-    pipe = Pipeline()
+    if not pipeline.INITIALIZED:
+        pipeline.init_enviroment()
     app.run(debug=True, port=5000, host='0.0.0.0')
