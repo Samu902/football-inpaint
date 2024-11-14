a6f985ef6f9dc31c8cea5cb579ec4f7ccc37ac75
Samuele Marrani <samuelemarrani@gmail.com>
2024-11-14 12:01:30 +0100

    interfaccia train lora completa

--- train-lora/src/Panel.tsx
+++ train-lora/src/Panel.tsx
@@ -8,109 +8,114 @@ interface InputPanelProps {
 
 export default function Panel(props: InputPanelProps) {
 
-    const [file, setFile] = useState<File>();
-    const [image, setImage] = useState(null);
+    const [inputFile, setInputFile] = useState<File>();
+    const [team, setTeam] = useState<string>();
+    const [steps, setSteps] = useState<number>();
+
+    const [parsedResultFile, setParsedResultFile] = useState(null);
+
     const [processing, setProcessing] = useState(false);
     const [error, setError] = useState(false);
     const [completed, setCompleted] = useState(false);
 
     props.modelApi.onProcessStart.push(onStartProcessing);
-    props.modelApi.onProcessEndWithSuccess.push(onEndProcessing);
+    props.modelApi.onProcessSuccess.push(onEndProcessing);
     props.modelApi.onProcessError.push(onErrorProcessing);
 
-    function onFileChange(e: ChangeEvent<HTMLInputElement>) {
+    function onInputFileChange(e: ChangeEvent<HTMLInputElement>) {
         if (!e.target.files)
             return;
 
-        setFile(e.target.files[0])
+        setInputFile(e.target.files[0])
         if (e.target.files[0]) {
-            const reader = new FileReader();
-            reader.onloadend = () => setImage(reader.result);
-            reader.readAsDataURL(e.target.files[0]);
-            props.modelApi.inputImage = e.target.files[0];
+            props.modelApi.inputFile = e.target.files[0];
         }
         else {
-            setImage(null);
-            props.modelApi.inputImage = null;
+            props.modelApi.inputFile = null;
         }
+    }
+
+    function onTeamChange(e: ChangeEvent<HTMLInputElement>) {
+        setTeam(e.target.value);
+        props.modelApi.team = e.target.value;
+    }
+
+    function onStepsChange(e: ChangeEvent<HTMLInputElement>) {
+        setSteps(parseInt(e.target.value));
+        props.modelApi.steps = parseInt(e.target.value);
     }
 
     function onProcessClick() {
-        props.modelApi.startImageProcessing();
+        props.modelApi.startLoRaProcessing();
     }
 
     function onStartProcessing() {
-        setProcessing(true);
-    }
-
-    function onEndProcessing() {
-        setProcessing(false);
-    }
-
-    function onErrorProcessing() {
-        setProcessing(false);
-    }
-
-    props.modelApi.onProcessStart.push(onImageStarted);
-    props.modelApi.onProcessEndWithSuccess.push(onImageCompleted);
-    props.modelApi.onProcessError.push(onImageError);
-
-    function onImageStarted() {
-        setImage(null);
+        setParsedResultFile(null);
         setProcessing(true);
         setError(false);
-    }
-    
-    function onImageCompleted(outputImage: string | ArrayBuffer) {
-        setProcessing(false);
-        setError(false);
-        setImage(outputImage);
+        setCompleted(false);
     }
 
-    function onImageError(error: string) {
-        setImage(null);
+    function onEndProcessing(outputFile: string | ArrayBuffer) {
+        setProcessing(false);
+        setError(false);
+        setParsedResultFile(outputFile);
+        setCompleted(true);
+    }
+
+    function onErrorProcessing(error: string) {
+        setParsedResultFile(null);
         setProcessing(false);
         setError(true);
+        setCompleted(false);
     }
 
     return (
         <Container className='my-4 px-5 py-4 border border-3 rounded-2'>
             <h5 className='text-center mt-2 mb-4'>Carica i dati per addestrare il modello di inpainting a disegnare nuove squadre e genera il file LoRa.</h5>
             <Form.Label className='mt-1'>File .zip con immagini di addestramento</Form.Label>
-            <Form.Control className='mb-4' type='file' accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*" onChange={onFileChange} />
+            <Form.Control className='mb-4' type='file' accept=".zip" onChange={onInputFileChange} />
             <Form.Label>Squadra</Form.Label>
-            <Form.Control className='mb-4' type='text' onChange={onFileChange} />
+            <Form.Control className='mb-4' type='text' onChange={onTeamChange} />
             <Form.Label>Iterazioni</Form.Label>
-            <Form.Control className='mb-4' type='number' min={0} onChange={onFileChange} />
-            <Row className='mt-4'>
-                <Col xs={3}>
-                    <Button type='button' className='btn btn-primary' disabled={!image || processing} onClick={onProcessClick}>Processa!</Button>
-                </Col>
-                <Col xs={9}>
-                    {
-                        processing &&
-                        <div className='position-absolute top-50 start-50 translate-middle'>
-                            <p>Attendi...</p>
-                            <Spinner animation="border" role="status" size='sm'>
-                                <span className="visually-hidden">Loading...</span>
-                            </Spinner>
-                        </div>
-                    }
-                    {
-                        error &&
-                        <div className='position-absolute top-50 start-50 translate-middle'>
-                            <p className='text-danger'>Errore durante l'elaborazione!</p>
-                        </div>
-                    }
-                    {
-                        completed &&
-                        <div className='position-absolute top-50 start-50 translate-middle'>
-                            <p className='text-danger'>Il tuo modello è pronto.</p>
-                            <Button type='button' className='btn btn-success' disabled={!image || processing} onClick={onProcessClick}>Scarica!</Button>
-                        </div>
-                    }
-                </Col>
-            </Row>
+            <Form.Control className='mb-4' type='number' min={0} onChange={onStepsChange} />
+            <div className='mt-4 d-flex'>
+                <Button type='button' className='btn btn-primary me-4' disabled={!inputFile || !team || steps == null || steps < 0 || processing} onClick={onProcessClick}>Processa!</Button>
+                {
+                    processing &&
+                    <span className='py-2'>
+                        Attendi...&nbsp;&nbsp;
+                        <Spinner animation="border" role="status" size='sm'>
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    </span>
+                }
+                {
+                    error &&
+                    <span className='py-2'>
+                        <span className='text-danger'>Errore durante l'elaborazione!</span>
+                    </span>
+                }
+                {
+                    completed &&
+                    <>
+                        {
+                            parsedResultFile? (
+                                <span>
+                                    Il tuo modello è pronto.&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a href={parsedResultFile}>
+                                        <Button type='button' className='btn btn-success'>Scarica!</Button>
+                                    </a>
+                                </span>
+                            ) : (
+                                <span className='py-2'>
+                                    <span className='text-danger'>Errore durante l'elaborazione!</span>
+                                </span>
+                            )
+                        }
+                    </>
+                }
+            </div>
         </Container>
     );
 }
